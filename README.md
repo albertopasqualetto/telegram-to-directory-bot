8a729411e5d8a5ecc7362bbb163e6287484e4710
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2025-01-02 23:21:34 +0000

    More generic + README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # telegram-to-directory-bot
 Telegram bot which recieves files and moves them to a specified destination directory
+
+Usage with docker compose (adapt it to your needs):
+```yaml
+# a telegram-bot-api server is needed to download big files
+telegram-bot-api:
+    image: aiogram/telegram-bot-api:latest
+    environment:
+      TELEGRAM_API_ID: "YOUR_API_ID"
+      TELEGRAM_API_HASH: "YOUR_API_HASH"
+      TELEGRAM_LOCAL: 1
+    volumes:
+      - telegram-bot-api-data:/var/lib/telegram-bot-api
+    ports:
+      - "8081:8081"
+
+telegram-to-directory-bot:
+    container_name: telegram-to-directory-bot
+    build:
+      context: ./telegram-to-directory-bot
+      dockerfile: Dockerfile
+      tags:
+        - "telegram-to-directory-bot:latest"
+    environment:
+      TG_BOT_API_SERVER: "http://telegram-bot-api:8081"
+      TG_BOT_TOKEN: "YOUR_BOT_TOKEN"
+      TG_FILTERS: "FILTER1|FILTER2|OTHER_FILTERS"   # Filters for the files to be moved, filter names are the same as here https://docs.python-telegram-bot.org/en/v21.6/telegram.ext.filters.html separated by "|"
+      UID: 1001 # UID of the user that will own the files, optional
+      GID: 988  # GID of the group that will own the files, optional
+      PERMISSIONS: "766" # Permissions to set for the files, optional
+      DESTINATION_NAME: "destination"   # Name of the destination directory; only used for tg answer message, optional
+    volumes:
+      - telegram-bot-api-data:/origin
+      - type: bind
+        source: /path/to/destination/on/host
+        target: /destination
+```
--- main.py
+++ main.py
@@ -15,9 +15,16 @@ logging.basicConfig(level=logging.INFO)
 ORIG_FOLDER = '/origin'
 DEST_FOLDER = '/destination'
 
-UID = os.environ.get('UID', 1001)
-GID = os.environ.get('GID', 988)
-PERMISSIONS = 0o766
+TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+TG_BOT_API_SERVER = os.environ.get('TG_BOT_API_SERVER')
+
+FILTERS = os.environ.get('TG_FILTERS', 'ALL').split('|')
+
+UID = int(os.environ.get('UID')) if os.environ.get('UID') else None
+GID = int(os.environ.get('GID')) if os.environ.get('UID') else None
+PERMISSIONS = int(os.environ.get('PERMISSIONS'), 8) if os.environ.get('PERMISSIONS') else None
+
+DESTINATION_NAME = os.environ.get('DESTINATION_NAME', 'destination')
 
 def format_filename(s):
     valid_chars = "-_(). %s%s" % (string.ascii_letters, string.digits)
@@ -50,8 +57,9 @@ async def file_received(update: Update, context: ContextTypes.DEFAULT_TYPE):
         dest_filename = f"{format_filename(update.effective_message.caption)}{ext}" if update.effective_message.caption else orig_filename.split('/')[-1]
         # logging.info(f"File destination: {dest_filename}")
         move_file_docker(orig_filename, dest_filename)
-        await reply_msg.edit_text(f"\u2714\uFE0F File moved to Jellyfin as: {dest_filename}")
+        await reply_msg.edit_text(f"\u2714\uFE0F File moved to {DESTINATION_NAME} as: {dest_filename}")
     except:
+        logging.error("An error occurred!", exc_info=True)
         await reply_msg.edit_text(f"\u274C An error occurred!")
 
 
@@ -59,25 +67,49 @@ def move_file_docker(orig_file_path, dest_file_path):
     shutil.move(f"{ORIG_FOLDER}/{orig_file_path}", f"{DEST_FOLDER}/{dest_file_path}")
     logging.info(f"File moved: {dest_file_path}")
     # Change ownership to the current user
-    os.chown(f"{DEST_FOLDER}/{dest_file_path}", UID, GID)
-    os.chmod(f"{DEST_FOLDER}/{dest_file_path}", PERMISSIONS)
-    logging.info(f"Changed ownership of {dest_file_path} to {UID}:{GID}, permissions: {PERMISSIONS}")
+    if UID is not None and GID is not None:
+        os.chown(f"{DEST_FOLDER}/{dest_file_path}", UID, GID)
+        logging.info(f"Changed ownership of {dest_file_path} to {UID}:{GID}")
+    if PERMISSIONS is not None:
+        os.chmod(f"{DEST_FOLDER}/{dest_file_path}", PERMISSIONS)
+        logging.info(f"Changed permissions of {dest_file_path} to {PERMISSIONS}")
 
 
 def main():
     app = ApplicationBuilder() \
         .read_timeout(100) \
         .write_timeout(100) \
-        .token(os.environ["TG_BOT_TOKEN"]) \
-        .base_url(os.environ["TG_BOT_API_SERVER"]+'/bot') \
-        .base_file_url(os.environ["TG_BOT_API_SERVER"]+'/file/bot') \
+        .token(TG_BOT_TOKEN) \
+        .base_url(TG_BOT_API_SERVER+'/bot') \
+        .base_file_url(TG_BOT_API_SERVER+'/file/bot') \
         .local_mode(True) \
         .build()
 
     app.add_handler(CommandHandler("start", start))
 
-    app.add_handler(MessageHandler(filters.VIDEO | filters.Document.VIDEO | filters.PHOTO | filters.Document.IMAGE, file_received))
-    app.add_handler(MessageHandler(~(filters.VIDEO | filters.Document.VIDEO | filters.PHOTO | filters.Document.IMAGE), lambda u, c: c.bot.send_message(chat_id=u.effective_chat.id, text="\u274C Filetype unsupported!\nPlease send a video or an image!")))
+    # Parse the filters
+    accepted_types = None
+    for filter_name in FILTERS:
+        if len(filter_name.split('.')) > 1:
+            filter_attr_cat = getattr(filters, filter_name.split('.')[0], None)
+            filter_attr = getattr(filter_attr_cat, filter_name.split('.')[1], None)
+        else:
+            filter_attr = getattr(filters, filter_name, None)
+        if filter_attr is None:
+            logging.info(f"Filter {filter_name} not found!")
+            continue
+        if accepted_types is None:
+            accepted_types = filter_attr
+        else:
+            accepted_types |= filter_attr
+        logging.info(f"Accepted types: {accepted_types}")
+
+
+    app.add_handler(MessageHandler(accepted_types, file_received))
+    app.add_handler(MessageHandler(~accepted_types, lambda u, c: (
+        c.bot.send_message(chat_id=u.effective_chat.id, text=f"\u274C Filetype unsupported!\nPlease send one among {" ".join(FILTERS)}!"),
+        logging.info(f"Received unsupported file type; message: {u.message}")
+    )))
 
     app.run_polling()
 
